62cf58779ee050451e77d921190f4ceb57665523
sapna-max <78731492+sapna-max@users.noreply.github.com>
2021-05-09 17:56:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,129 @@
+const express = require('express');
+const app = express();
+const port = 9700;
+
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient
+// const mongourl = "mongodb://localhost:27017";
+const mongourl = "mongodb+srv://admin123:admin21@cluster0.pvatv.mongodb.net/edurekinternship?retryWrites=true&w=majority"
+let db;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+// middleware
+app.use(cors());
+// use in post call to parse the data
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json())
+
+
+// first route (default)
+app.get('/',function(req,res){
+    res.send("Hi from express hiiiiii")
+})
+
+app.get('/location',(req,res) => {
+    db.collection('location').find().toArray((err,result) => {
+       // if(err) throw err;
+        res.send(result)
+    })
+})
+
+app.get('/rest/:id',(req,res) => {
+    var id = req.params.id
+    //let {id} = req.params;
+    db.collection('restaurant').find({_id:id}).toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+app.get('/restaurent',(req,res) => {
+    var condition = {};
+    let sortcondition = {cost:1}
+    if(req.query.sort){
+        sortcondition = {cost:Number(req.query.sort)}
+    }
+
+    //meal+city
+    if(req.query.city && req.query.mealtype){
+        condition={$and:[{"type.mealtype":req.query.mealtype},{city:req.query.city}]}
+    }
+    //meal+cuisine
+    else if(req.query.cuisine && req.query.mealtype){
+        condition={$and:[{"type.mealtype":req.query.mealtype},{"Cuisine.cuisine":req.query.cuisine}]}
+    }
+    // city
+    else if(req.query.city){
+        condition = {city:req.query.city}
+    }
+    // mealtype
+    else if(req.query.mealtype){
+        condition = {"type.mealtype":req.query.mealtype}
+    }
+    // cuisine
+    else if(req.query.cuisine){
+        condition = {"Cuisine.cuisine":req.query.cuisine}
+    }
+    // cost
+    else if(req.query.lcost && req.query.hcost){
+        condition = {$and:[{cost:{$lt:Number(req.query.hcost),$gt:Number(req.query.lcost)}}]}
+    }
+    db.collection('restaurant').find(condition).sort(sortcondition).toArray((err,result) => {
+        res.send(result)
+    })
+})
+
+
+//  mealtype
+app.get('/mealtype',(req,res) => {
+    db.collection('mealType').find().toArray((err,result) => {
+        res.send(result)
+    })
+})
+
+//  cuisine
+app.get('/cuisine',(req,res) => {
+    db.collection('cuisine').find().toArray((err,result) => {
+        res.send(result)
+    })
+})
+
+
+//  viewBooking
+app.get('/orders',(req,res) => {
+    db.collection('orders').find().toArray((err,result) => {
+        res.send(result)
+    })
+})
+
+
+// place order
+app.post('/placeOrder',(req,res) => {
+    db.collection('orders').insert(req.body,(err,result) => {
+        if(err) throw err;
+        res.send('data added')
+    })
+}) 
+// second route (default)
+app.get('/second',function(req,res){
+    res.send("Hi to second URL")
+})
+
+MongoClient.connect(mongourl,{useUnifiedTopology: true} ,(err,connection)=>{
+   if(err) throw err;
+    // provide database name
+  console.log("connection done")
+    db = connection.db('edurekinternship')
+})
+
+app.listen(port,function(err){
+   if(err) throw err;
+   console.log(`Server is running on port ${port}`)
+})
+
+
+  //restaurant?lcost=200&hcost=400
+  //restaurant?city=3
+  //restaurant?city=1&mealtype=2
